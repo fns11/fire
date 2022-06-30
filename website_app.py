7b413ee7c1424942aa32945af17afb7a28708165
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-29 23:21:32 -0400

    Update website_app.py

--- website_app.py
+++ website_app.py
@@ -31,6 +31,30 @@ if choose == "About":
         st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
     with col2:               # To display brand log
         st.image(logo, width=130 )
+        
+        <div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
     
     st.write("Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
     st.image(profile, width=400 )
